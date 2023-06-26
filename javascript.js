eec8d335a14dce16388ab2c7bcd066e4b80ce14c
Ravindu Hasanka <rhatu2000@gmail.com>
2023-06-26 12:17:56 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+function check(){
+    alert("check");
+}
+
+function add(text1,text2){
+    alert(text1.concat(text2));
+}
+
+function lower(text){
+    alert(text.toLowerCase());
+}   
+
+function upper(text){
+    alert(text.toUpperCase());
+}
+
+function splittext(text){
+    var arr = text.split(',')
+    alert(arr);
+}
