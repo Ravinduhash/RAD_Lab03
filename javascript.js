68ab2e76b34bfb9985f91ca715c72d71c8ba9dac
Ravindu Hasanka <rhatu2000@gmail.com>
2023-06-27 18:20:46 +0530

    activity02 done

--- javascript.js
+++ javascript.js
@@ -41,3 +41,22 @@ function and(x,y){
     return x&&y
 }
 
+function q2(num){
+    alert("Type of the variable is "+ typeof(num));
+}
+
+function q3(){
+    const currentDate = new Date();
+    alert("Today is "+ currentDate+"\nThe data type of currentDate is "+ typeof(currentDate));
+}
+
+function q4(){
+    var text = "OceanGate";
+    if(text.length>10){
+        alert("The length of the text is greater than 10");
+    }
+    else{
+        alert("The length of the text is not greater than 10");
+    }
+    
+}
